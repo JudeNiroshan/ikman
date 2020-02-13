883dcb997602511516484f6b9e66b2e2c6d2f604
jude.niroshan11@gmail.com <jude.niroshan11@gmail.com>
2020-02-13 21:12:12 +0100

    path variable correction

--- src/main/java/com/ikman/app/ikman/AdController.java
+++ src/main/java/com/ikman/app/ikman/AdController.java
@@ -2,29 +2,32 @@ package com.ikman.app.ikman;
 
 import com.ikman.app.ikman.models.Ad;
 import com.ikman.app.ikman.repository.AdRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 public class AdController {
-    @Autowired
-    AdRepository adRepository;
+    final AdRepository adRepository;
+
+    public AdController(AdRepository adRepository) {
+        this.adRepository = adRepository;
+    }
 
     @GetMapping("/health")
-    public String health(){
+    public String health() {
         return "App is working";
     }
 
-    @GetMapping("/ad/{name}")
-    public Ad getByName(String adName){
-        return adRepository.findByName(adName).stream().findFirst().orElse(new Ad());
+    @GetMapping("/ads/{name}")
+    public Ad getByName(@PathVariable String name) {
+        return adRepository.findByName(name).stream().findFirst().orElse(new Ad());
     }
 
     @GetMapping("/ads")
-    public List<Ad> allAds(){
+    public List<Ad> allAds() {
         return adRepository.findAll();
     }
 }
